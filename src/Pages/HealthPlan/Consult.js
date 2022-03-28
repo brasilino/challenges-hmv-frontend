7ed43fb6a8bb6d97d69f7fff1580c37440d05adc
Alan <alan_brasilino@icloud.com>
2022-03-27 21:49:22 -0300

    Corrige mensagens de warnings

--- src/Pages/HealthPlan/Consult.js
+++ src/Pages/HealthPlan/Consult.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Row, Col, Card, Table, Tabs, Tab} from 'react-bootstrap';
+import {Row, Col, Card, Table} from 'react-bootstrap';
 import Moment from 'moment';
 
 import Aux from "../../hoc/_Aux";
--- src/routes.js
+++ src/routes.js
@@ -5,22 +5,22 @@ window.jQuery = $;
 window.$ = $;
 global.jQuery = $;
 
-const DashboardDefault = React.lazy(() => import('./Demo/Dashboard/Default'));
+// const DashboardDefault = React.lazy(() => import('./Demo/Dashboard/Default'));
 
-const UIBasicButton = React.lazy(() => import('./Demo/UIElements/Basic/Button'));
-const UIBasicBadges = React.lazy(() => import('./Demo/UIElements/Basic/Badges'));
-const UIBasicBreadcrumbPagination = React.lazy(() => import('./Demo/UIElements/Basic/BreadcrumbPagination'));
+// const UIBasicButton = React.lazy(() => import('./Demo/UIElements/Basic/Button'));
+// const UIBasicBadges = React.lazy(() => import('./Demo/UIElements/Basic/Badges'));
+// const UIBasicBreadcrumbPagination = React.lazy(() => import('./Demo/UIElements/Basic/BreadcrumbPagination'));
 
-const UIBasicCollapse = React.lazy(() => import('./Demo/UIElements/Basic/Collapse'));
-const UIBasicTabsPills = React.lazy(() => import('./Demo/UIElements/Basic/TabsPills'));
-const UIBasicBasicTypography = React.lazy(() => import('./Demo/UIElements/Basic/Typography'));
+// const UIBasicCollapse = React.lazy(() => import('./Demo/UIElements/Basic/Collapse'));
+// const UIBasicTabsPills = React.lazy(() => import('./Demo/UIElements/Basic/TabsPills'));
+// const UIBasicBasicTypography = React.lazy(() => import('./Demo/UIElements/Basic/Typography'));
 
-const FormsElements = React.lazy(() => import('./Demo/Forms/FormsElements'));
-const BootstrapTable = React.lazy(() => import('./Demo/Tables/BootstrapTable'));
-const Nvd3Chart = React.lazy(() => import('./Demo/Charts/Nvd3Chart/index'));
-const GoogleMap = React.lazy(() => import('./Demo/Maps/GoogleMap/index'));
-const OtherSamplePage = React.lazy(() => import('./Demo/Other/SamplePage'));
-const OtherDocs = React.lazy(() => import('./Demo/Other/Docs'));
+// const FormsElements = React.lazy(() => import('./Demo/Forms/FormsElements'));
+// const BootstrapTable = React.lazy(() => import('./Demo/Tables/BootstrapTable'));
+// const Nvd3Chart = React.lazy(() => import('./Demo/Charts/Nvd3Chart/index'));
+// const GoogleMap = React.lazy(() => import('./Demo/Maps/GoogleMap/index'));
+// const OtherSamplePage = React.lazy(() => import('./Demo/Other/SamplePage'));
+// const OtherDocs = React.lazy(() => import('./Demo/Other/Docs'));
 
 const HealthPlanRegister = React.lazy(() => import('./Pages/HealthPlan/Register'));
 const HealthPlanConsult = React.lazy(() => import('./Pages/HealthPlan/Consult'));
