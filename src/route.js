e5ad22c62c18eccc4ebfe054b1404159be3c37ce
Alan <alan_brasilino@icloud.com>
2022-03-27 21:30:54 -0300

    Adiciona página de login

--- src/route.js
+++ src/route.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const SignUp1 = React.lazy(() => import('./Demo/Authentication/SignUp/SignUp1'));
-const Signin1 = React.lazy(() => import('./Demo/Authentication/SignIn/SignIn1'));
+const SignIn = React.lazy(() => import('./Pages/Authentication/SignIn'));
 
 const route = [
     { path: '/auth/signup-1', exact: true, name: 'Signup 1', component: SignUp1 },
-    { path: '/auth/signin-1', exact: true, name: 'Signin 1', component: Signin1 }
+    { path: '/auth/signin', exact: true, name: 'Signin', component: SignIn }
 ];
 
 export default route;
