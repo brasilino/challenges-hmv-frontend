92fbb9213e97dfcdc72f525ad26004eba3a7b07e
Alan <alan_brasilino@icloud.com>
2022-04-02 21:50:54 -0300

    Remove itens e arquivos não utilizados

--- src/route.js
+++ src/route.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-const SignUp1 = React.lazy(() => import('./Demo/Authentication/SignUp/SignUp1'));
 const SignIn = React.lazy(() => import('./Pages/Authentication/SignIn'));
 
 const route = [
-    { path: '/auth/signup-1', exact: true, name: 'Signup 1', component: SignUp1 },
     { path: '/auth/signin', exact: true, name: 'Signin', component: SignIn }
 ];
 
